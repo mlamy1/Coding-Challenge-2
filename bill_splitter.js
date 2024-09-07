6c98a8f2d1f54d13acfbd15a1fd79a2fb4228d84
Myia Lamy <mlamy@usf.edu>
2024-09-07 19:02:28 -0400

    Initial logic for calculating tip using ternary operator

---
+++ bill_splitter.js
@@ -0,0 +1,11 @@
+// Task 1: Calculating the Tip
+
+var  x = 88; // X = bill  
+
+var tip = console.log(x >= 50 && x <= 300 ? x *= .15 : x *= .20);  
+// If the bill is between 50 and 300, tip is 15%.  Otherwise, the tip is 20%. 
+
+
+
+
+
